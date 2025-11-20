0b5d57262a58441024e95de184b234eaa435e44d
gkm <gkm81628@gmail.com>
2025-11-20 15:56:52 +0800

    Add alias config to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: "/tucheng-qingcao/", // GitHub Pages 部署路徑
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
